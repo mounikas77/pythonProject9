122345e1310b3f3644572f7d8280984522a2d80c
mounikas77 <mounikabujji18@gmail.com>
2024-04-23 17:25:15 +0530

    django project practice

---
+++ main.py
@@ -0,0 +1,25 @@
+#global variable example in w3 school
+# x="python programming"
+# def myfunction():
+#     x="python is a popular programming"
+#     print(x)
+# myfunction()
+# print(x)
+# o/p:
+# python is a popular programming
+# python programming
+
+################################
+# x="python"
+# def myfun():
+#     #global x
+#     x="amozon"
+#     print("python is "+x)
+# myfun()
+# print("python is " +x)
+# o/p:
+# python is amozon
+# python is python
+##########################################
+
+
